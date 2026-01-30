d6a2cb2e3b64aa497b7149d1aabdea4812be334b
SujitDevl <pandasujit687@gmail.com>
2026-01-30 23:45:59 +0530

    rearrange contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../components/layout/Container";
 
 function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
   return (
     <Container>
       <h1 className="text-3xl font-bold py-10">Contact Us</h1>
